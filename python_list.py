432d7ee9e3dd35e85bffeb6f121b74b4c479e9f8
Able <munirammar0@gmail.com>
2023-08-02 23:07:28 +0500

    Basic Intro to list

---
+++ python_list.py
@@ -0,0 +1,10 @@
+# Basic Syntax for creating Python List is
+list_p = []
+# we can also set data type of a variable as list by:
+lis_A = list()
+
+#let's check data types of both variables:
+print(type(list_p))
+print(type(lis_A))
+#output:                <class 'list'>      <class 'list'>
+
